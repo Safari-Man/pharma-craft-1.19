ddb0c4b45c367b169139787cdea3d7490e75f885
Safari-Man <allencarter615@gmail.com>
2022-12-20 18:08:19 -0800

    Initial commit

---
+++ src/main/java/net/dique/pharmacraft/PharmaCraftClient.java
@@ -0,0 +1,10 @@
+package net.dique.pharmacraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PharmaCraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
